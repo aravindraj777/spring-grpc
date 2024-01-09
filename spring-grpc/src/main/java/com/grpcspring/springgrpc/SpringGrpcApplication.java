51a9419ecc73bba50823b392868c3f9378b85de2
Aravind <aravindmunna240@gmail.com>
2024-01-09 14:49:35 +0530

    Initial commit

---
+++ spring-grpc/src/main/java/com/grpcspring/springgrpc/SpringGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.grpcspring.springgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcApplication.class, args);
+	}
+
+}
